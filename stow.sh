3037cd02abe253dfcff06091905d7d304c9ab6cf
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-05-07 20:31:12 +0300

    Initial stow logic

---
+++ stow.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+configs=(
+  alacritty
+  git
+)
+
+target_dir=~
+
+for c in ${configs[@]}
+do
+  echo "${Blue}===> ${White}Creating config symlinks for $c..."
+  stow -t $target_dir $c
+done
