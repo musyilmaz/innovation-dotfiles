20253db1e4e9368dca6ba504dfe81a056d5c9f0e
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2025-02-03 12:38:35 +0300

    fix(nvim): fix nvim-chainsaw

--- nvim/.config/atlas/lua/atlas/plugins/bufferline.lua
+++ nvim/.config/atlas/lua/atlas/plugins/bufferline.lua
@@ -17,7 +17,7 @@ return {
           },
           diagnostics = "nvim_lsp",
           diagnostics_update_on_event = true,
-          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          diagnostics_indicator = function(count, level, _, _)
             local icon = level:match("error") and " " or " "
             return " " .. icon .. count
           end,
--- nvim/.config/atlas/lua/atlas/plugins/editing-support.lua
+++ nvim/.config/atlas/lua/atlas/plugins/editing-support.lua
@@ -1,6 +1,7 @@
 return {
   {
     "chrisgrieser/nvim-chainsaw",
+    event = "VeryLazy",
     opts = {
       marker = "❗",
       logStatements = {
@@ -11,15 +12,56 @@ return {
       },
     },
     keys = {
-			-- stylua: ignore start
-			{"<leader>lb", function() require("chainsaw").beepLog() end, desc = "󰸢 beep" },
-			{"<leader>ll", function() require("chainsaw").variableLog() end, mode = {"n", "x"}, desc = "󰸢 variable" },
-			{"<leader>lo", function() require("chainsaw").objectLog() end, mode = {"n", "x"}, desc = "󰸢 object" },
-			{"<leader>lm", function() require("chainsaw").messageLog() end, desc = "󰸢 message" },
-			{"<leader>lt", function() require("chainsaw").timeLog() end, desc = "󰸢 time" },
+      {
+        "<leader>ll",
+        function()
+          require("chainsaw").variableLog()
+        end,
+        mode = { "n", "x" },
+        desc = "󰸢 variable",
+      },
+      {
+        "<leader>lo",
+        function()
+          require("chainsaw").objectLog()
+        end,
+        mode = { "n", "x" },
+        desc = "󰸢 object",
+      },
+      {
+        "<leader>lm",
+        function()
+          require("chainsaw").messageLog()
+        end,
+        desc = "󰸢 message",
+      },
+      {
+        "<leader>lt",
+        function()
+          require("chainsaw").timeLog()
+        end,
+        desc = "󰸢 time",
+      },
 
-			{"<leader>lr", function() require("chainsaw").removeLogs() end, desc = "󰹝 remove logs" },
-      -- stylua: ignore end
+      {
+        "<leader>lr",
+        function()
+          require("chainsaw").removeLogs()
+        end,
+        desc = "󰹝 remove logs",
+      },
     },
+    config = function()
+      require("chainsaw").setup({
+        logStatements = {
+          variableLog = {
+            javascript = 'console.log("{{marker}} {{filename}}:{{lnum}} -- {{var}}:", {{var}});',
+          },
+          objectLog = {
+            javascript = 'console.log("{{marker}} {{filename}}:{{lnum}} -- {{var}}:", JSON.stringify({{var}}, null, 2))',
+          },
+        },
+      })
+    end,
   },
 }
