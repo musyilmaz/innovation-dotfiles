02839851a4ce6d6976a3519578d802e894123c81
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2025-04-03 13:20:01 +0300

    update markdown.lua

--- nvim/.config/atlas/lua/atlas/plugins/markdown.lua
+++ nvim/.config/atlas/lua/atlas/plugins/markdown.lua
@@ -1,6 +1,8 @@
 return {
   "MeanderingProgrammer/markdown.nvim",
   main = "render-markdown",
-  opts = {},
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
 }
