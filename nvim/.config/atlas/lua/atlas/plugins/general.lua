85c13b67747c9980f64ceec256fba65c7077dc87
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2025-04-07 16:20:14 +0300

    feat: implement avante back again

--- nvim/.config/atlas/lua/atlas/plugins/general.lua
+++ nvim/.config/atlas/lua/atlas/plugins/general.lua
@@ -47,4 +47,35 @@ return {
       })
     end,
   },
+
+  -- ALPHA NVIM
+
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local startify = require("alpha.themes.startify")
+
+      startify.section.header.val = {
+        "  █████╗ ████████╗██╗      █████╗ ███████╗ ",
+        " ██╔══██╗╚══██╔══╝██║     ██╔══██╗██╔════╝ ",
+        " ███████║   ██║   ██║     ███████║███████╗ ",
+        " ██╔══██║   ██║   ██║     ██╔══██║╚════██║ ",
+        " ██║  ██║   ██║   ███████╗██║  ██║███████║ ",
+        " ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝ ",
+      }
+
+      startify.section.top_buttons.val = {
+        startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      }
+
+      startify.file_icons.provider = "devicons"
+
+      alpha.setup(startify.opts)
+
+      vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
+  },
 }
